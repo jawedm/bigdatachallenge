1a58f080c0402c50a8fd275793b310feda1efcfe
Grant Hutchison <grant.hutchison@gmail.com>
2015-01-08 12:23:51 -0500

    updated scripts

--- rscripts/analysis.R
+++ rscripts/analysis.R
@@ -1,5 +1,5 @@
-
 library(doBy)
+library(ggplot2)
 
 #IMPORTING CSV FILES AND ASSIGNING VARIABLE
 val <- read.csv("VALDATA.csv", header=TRUE)
@@ -8,19 +8,27 @@ target <- read.csv("TARGET.csv", header=TRUE)
 stores <- read.csv("STORES.csv", header=TRUE)
 customers <- read.csv("CUSTOMERS.csv", header=TRUE)
 
-#STORES TOTAL SUM
-sum(val$SALES[val[,"STORE_ID"] == 20001319])
-sum(val$SALES[val[,"STORE_ID"] == 20001321])
-sum(val$SALES[val[,"STORE_ID"] == 20001323])
-sum(val$SALES[val[,"STORE_ID"] == 20004029])
-sum(val$SALES[val[,"STORE_ID"] == 20004053])
+all.tran <- merge(val,tran,all=TRUE)
+tran <-all.tran
 
-#STORES 2012 SUM
-sum(val[ val$STORE_ID == 20001319 & val$YEAR == 2012, 10])
-sum(val[ val$STORE_ID == 20001321 & val$YEAR == 2012, 10])
-sum(val[ val$STORE_ID == 20001323 & val$YEAR == 2012, 10])
-sum(val[ val$STORE_ID == 20004029 & val$YEAR == 2012, 10])
-sum(val[ val$STORE_ID == 20004053 & val$YEAR == 2012, 10])
+# 2012 Analysis
+tran2012<-tran[tran$YEAR==2012,]
+store.sales.2012 <- summaryBy(SALES~YEAR+STORE_ID+WEEK, data=tran2012, FUN=c(sum))
+
+store.sales.2012$STORE <- factor(store.sales.2012$STORE_ID)
+
+p <- ggplot(store.sales.2012, aes(x = WEEK, y = SALES.sum, color = STORE))
+p + geom_line() + ylab("SALES") + ggtitle("2012 Sales")
+
+#2013 Analysis
+tran2013<-tran[tran$YEAR==2013,]
+store.sales.2013 <- summaryBy(SALES~YEAR+STORE_ID+WEEK, data=tran2013, FUN=c(sum))
+
+store.sales.2013$STORE <- factor(store.sales.2013$STORE_ID)
+
+p <- ggplot(store.sales.2013, aes(x = WEEK, y = SALES.sum, color = STORE))
+p + geom_line() + ylab("SALES") + ggtitle("2013 Sales")
+
 
 #STORE 20001319 SUM BETWEEN WEEK 20 AND 30 IN 2013
 store1Data2012 <- sum(val[val$STORE_ID == 20001319 & val$YEAR == 2012 & val$WEEK >= 20 & val$WEEK <= 30, 10])
@@ -32,8 +40,19 @@ customersStore1
 
 tran_store1 <- tran[tran$STORE_ID=='20001319' & tran$YEAR==2011,]
 sum.store1 <- summaryBy(SALES~YEAR+STORE_ID+WEEK+CAT_DESC, data=tran_store1, FUN=c(sum))
-library(ggplot2)
+
 qplot(factor(WEEK), SALES.sum, data = sum.store1, geom=c("boxplot", "jitter"))
 d <- ggplot(sum.store1, aes(WEEK, SALES.sum)) + stat_binhex(na.rm = TRUE) + theme(aspect.ratio = 1)
 d + facet_wrap(~ CAT_DESC)
 
+
+
+#LOOKING AT DATA FROM STORE 20001319 IN 2012 ONLY
+combined <- merge(customers, tran, by.y="CUSTOMER_ID")
+store1v1  <- combined[combined$STORE_ID == 20001321 & combined$YEAR == 2012, ]  
+store1v1$POSTCODE <- substr(store1v1$POSTCODE, 1, 3)
+sum.store1v1 <- summaryBy(SALES~YEAR+POSTCODE, data=store1v1, FUN=c(sum))
+
+ggplot(sum.store1v1, aes(POSTCODE, SALES.sum)) +
+  geom_point(shape=1) +    # Use hollow circles
+  geom_smooth(method=lm) 
